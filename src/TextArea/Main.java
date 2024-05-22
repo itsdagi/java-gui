0691bcab6970f1ecba2e51ad1279a78892b7d05e
Dagim Alemu <dagimalemux@gmail.com>
2024-05-22 18:08:59 +0300

    the button listner not working

--- src/TextArea/Main.java
+++ src/TextArea/Main.java
@@ -1,4 +1,7 @@
 package TextArea;
 
 public class Main {
+    public static void main(String[] args){
+        TheText theText = new TheText();
+    }
 }
--- src/TextArea/TheText.java
+++ src/TextArea/TheText.java
@@ -4,37 +4,42 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class TheText implements ActionListener{
-    public static void main(String[] args){
-        JFrame frame = new JFrame();
-        JPasswordField password = new JPasswordField();
-        password.setPreferredSize(new Dimension(100, 40));
-        password.setEchoChar('*');
-        password.setBackground(Color.lightGray);
+public class TheText extends JFrame implements ActionListener {
+   // JPasswordField password;
+    JTextField text;
+    JButton button;
+    TheText() {
 
-        JTextField text = new JTextField();
-        text.setPreferredSize(new Dimension(100, 60));
+//        new JPasswordField();
+//        password.setPreferredSize(new Dimension(100, 40));
+//        password.setEchoChar('*');
+//        password.setBackground(Color.lightGray);
+
+        TextField text = new TextField();
+        text.setPreferredSize(new Dimension(100, 40));
         text.setBackground(Color.lightGray);
-        JButton button = new JButton("Submit");
+
+        JButton button =   new JButton("Submit");
         button.setPreferredSize(new Dimension(100, 30));
         button.setFocusable(false);
         button.addActionListener(this);
 
 
-        frame.setVisible(true);
-        frame.setLayout(new BoxLayout( frame.getContentPane(), BoxLayout.Y_AXIS));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.add(text);
-        frame.add(password);
-        frame.pack();
-
+        this.setVisible(true);
+        this.setLayout(new FlowLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.add(text);
+       // this.add(password);
+        this.add(button);
+        this.pack();
+    }
+        @Override
         public void actionPerformed(ActionEvent e) {
-            if(e.getSource() == button){
-                System.out.println("Welcome" + text.getText() + "Your password is: " + password.getText());
+            if (e.getSource() == button) {
+                System.out.println(text.getText());
             }
+        }
     }
 
-
-}
 
